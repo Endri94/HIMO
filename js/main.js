2f0862f9a4c1211a3b926dcabc19891c17e00bd8
Endri Grigoryan <162798482+Endri94@users.noreply.github.com>
2024-03-19 21:06:52 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// swiper
+$(function () {
+  $('.bike__slider, .slider__items').slick({
+    dots: true,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
